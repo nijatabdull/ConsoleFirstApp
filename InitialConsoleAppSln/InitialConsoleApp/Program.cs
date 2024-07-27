92f80378dfb894baca9da1733c0d4bc3abc075dc
nijatabdull <nijattttsa@gmail.com>
2024-07-27 12:28:46 +0400

    Add initial commit

---
+++ InitialConsoleAppSln/InitialConsoleApp/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace InitialConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // PascalCase
+            // camelCase - variables,
+
+            // Number Section 
+            long bigNumber = -10000000;
+            int userAge = -100000;
+            //short id = -2000;
+            byte num = 5;
+
+            ulong bigNumber2 = -2;
+            uint num2 = -3;
+            ushort num3 = -4;
+            sbyte num4 = -5;
+
+            // Floating
+            float floatType1 = 5.3F;
+            double doubleType = 15.3;
+            decimal decimalType = 24.5m;
+
+            // Logical 
+            bool boolType;
+
+            // Char, // ASCII
+            char charType;
+
+            // String
+            string name = "";
+
+            // DateTime
+            DateTime dateTime = new DateTime(2024, 7, 24);
+
+            // Computer
+            string computerName = "Acer";
+            byte ramSize = 12;
+            DateTime releaseDate = new DateTime(2018, 2, 15);
+            bool hasSdd = true;
+
+            // Car
+            string carName = "Kia";
+
+            // Person
+            string personName = "Rasul";
+            string personSurname = "Rashidov";
+            byte personAge = 28;
+            byte weight = 79;
+            byte height = 175;
+            int id = 4058695;
+            string personEmail = "rasulrashidof@mail.ru";
+            string phoneNumber = "+99455-445-04-78";
+          
+            
+
+            
+
+        }
+    }
+}
---
+++ InitialConsoleAppSln/SecondConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SecondConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = 10;
+            int num2 = 3;
+            int result = num1 * num2;
+            Console.WriteLine("Result: " + result);
+        }
+    }
+}
